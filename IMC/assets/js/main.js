cb2351dc7d5489752e30a316d59919a56eb8d32d
viniciogomes92 <viniciogomes@gmail.com>
2024-09-24 10:36:03 -0300

    Update main.js

--- IMC/assets/js/main.js
+++ IMC/assets/js/main.js
@@ -1,20 +1,25 @@
 function calculaIMC (){
+  //Puxo as TAG's HTML de classe form e resultado para os objetos de mesmo nome
   const form = document.querySelector('.form');
   const resultado = document.querySelector('.resultado');
   
+  //Faço o envio do Form com a função sendForm
   function sendForm(event){
     event.preventDefault();
 
+    //Trago os dados dos inputs contidos no objeto form
     const nome = form.querySelector('.inputNome');
     const sobrenome = form.querySelector('.inputSobrenome');
     const peso = form.querySelector('.inputPeso');
     const altura = form.querySelector('.inputAltura');
 
+    //Verifico se os campos estão vazios
     if (!nome.value || !sobrenome.value || !peso.value || !altura.value){
       resultado.innerHTML = `<p class="msgErro">Por favor, preencha todos os campos corretamente.</p>`;
       return;
     }
 
+    //Crio o objeto pessoa para armazenar os dados oriundos do form do usuário
     const pessoaIMC = ({
       nome: nome.value,
       sobrenome: sobrenome.value,
@@ -24,6 +29,8 @@ function calculaIMC (){
 
     let imc;
 
+    /* Verifico se a altura informada é um número, caso não seja retorno uma msg de erro
+    e caso seja verifico ainda se a altura informada é um número real ou inteiro para tratar o valor da forma correta.*/
     if (!pessoaIMC.altura === true) {
       resultado.innerHTML = `<p class="msgErro">Por favor, preencha todos os campos corretamente.</p>`;
       return;
@@ -33,11 +40,9 @@ function calculaIMC (){
     } else {
       imc = pessoaIMC.peso / (pessoaIMC.altura * pessoaIMC.altura);  
     }
-    
-    console.log(pessoaIMC.altura);
-    console.log(imc);
-    //const imc = pessoaIMC.peso / (pessoaIMC.altura * pessoaIMC.altura);
 
+
+    //Aqui verifico o valor do IMC para ver em qual faixa ele se encaixa e retornar a informação ao usuário através do objeto resultado que é uma DIV HTML
     if (imc.toFixed(1) < 18.5){
       resultado.innerHTML = `<p>${pessoaIMC.nome} ${pessoaIMC.sobrenome}, seu IMC é ${imc.toFixed(1)} e, portanto, você está abaixo do peso.</p>`;
     } else if ( imc.toFixed(1) >= 18,5 && imc.toFixed(1) <= 24.9 ){
@@ -52,6 +57,8 @@ function calculaIMC (){
       resultado.innerHTML = `<p>${pessoaIMC.nome} ${pessoaIMC.sobrenome}, seu IMC é ${imc.toFixed(1)} e, portanto, você está com Obesidade Grau 3.</p>`;
     }
   }
+  
+  //Aqui aguardamos o evento submit do HTML para executar a função sendForm
   form.addEventListener('submit', sendForm);
 }
 
